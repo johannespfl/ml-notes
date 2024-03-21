1efba1f95349f361bd0bd68fa96b54cea75a2c66
Johannes Pflugmacher <johannes.pflugmacher@mgm-tp.com>
2024-03-21 16:29:31 +0100

    add first chapter example for hf nlp

---
+++ courses/hf-nlp/hf_nlp/chapter01.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis")
+classifier("I've been waiting for a HuggingFace course my whole life.")
